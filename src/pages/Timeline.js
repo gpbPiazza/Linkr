c3c2ab9ec8c5f5218bbb2310ac8df609e3570316
Gabriel Milhomem <gabriell.mil@gmail.com>
2020-10-28 16:45:40 -0300

    Chore: alerts error server in getPosts

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -6,13 +6,14 @@ import Header from  '../components/Header';
 import Trending from "../components/Trending";
 import Publish from "../components/Publish";
 import Loading from '../components/Loading';
-import {Main, Title} from '../components-style/cmpnt-styles';
+import {Main, Title, Error} from '../components-style/cmpnt-styles';
 import LoginContext from "../context/LoginContext";
 import Posts from "../components/Posts";
 
-
 const Timeline = () => {
     const [posts, setPosts] = useState([]);
+    const [error, setError] = useState('');
+    const [booleanError, setBooleanError] = useState(false);
     const {userForm, controlForm} = useContext(LoginContext);
     const {userRegister, config, clearUser} = userForm;
     const {loading, setLoading} = controlForm;
@@ -25,19 +26,26 @@ const Timeline = () => {
         requestGetPost();
     }
     
-
     //REQUEST GET NA TIME LINE PADRÃO
     const requestGetPost = () => {
         setLoading(true);
-        const request = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v1/linkr/posts?offset=0&limit=2',{headers : config.headers});
+        const request = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v1/linkr/posts?offset=0&limit=2', config);
 
         request.then(({data}) => {
             console.log(data, 'RESPOSTA SUCESSO DA API GET POSTS');
+            setLoading(false);
+            if(data.posts.length === 0) {
+                setError('Nenhum post encontrado');
+                setBooleanError(true);
+            }
             setPosts(data.posts);
+            
         });
 
         request.catch(({response}) => {
             setLoading(false);
+            setError('Houve uma falha ao obter os posts, por favor atualize a página');
+            setBooleanError(true);
             console.log(response, 'RESPOSTA ERROR DA API');
         }); 
     } 
@@ -49,17 +57,20 @@ const Timeline = () => {
             <Header />
             <Title> timeline </Title>
             <ContainerLinkdr>
+                
                 <Publish getPosts={getPosts}/>            
                 {loading ? 
                         <ContainerLoading>
                             <Loading />
                         </ContainerLoading>
                     :
-                    <>
+                    booleanError ?
+                        <Error fontSize= {'2rem'}> {(error) ? error : ''} </Error>
+                        :
                         <ContainerPosts>
                             {posts.map((post) => <Posts post={post} key={post.id}/>)}
                         </ContainerPosts>
-                    </>}
+                }
             </ContainerLinkdr>
             <ContainerTrending>
                 <Trending />
@@ -77,7 +88,9 @@ const ContainerTrending = styled.div`
 
 const ContainerLinkdr = styled.div`
     width: 62%;
+    text-align: center;
 `;
+
 const ContainerPosts = styled.div`
     width: 100%;
 `;
